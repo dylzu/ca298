21f2da9f2b809a6bfcc4497ecd2df517e31763d2
dylan corbari <dylan.corbari2@mail.dcu.ie>
2022-04-20 21:55:43 +0100

    finished web app

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dylans Shop' });
 });
 
 router.get('/login', function(req, res, next){
@@ -17,5 +17,22 @@ router.get('/logout', function(req, res, next){
 router.get('/register', function(req, res, next){
   res.render('register')
 });
+
+router.get('/productindividual', function(req, res, next){
+  res.render('productindividual')
+});
+
+router.get('/basket', function(req, res, next){
+  res.render('basket')
+});
+
+router.get('/order-confirmation', function(req, res, next){
+  res.render('order-confirmation')
+});
+
+router.get('/previous-orders', function(req, res, next){
+  res.render('previous-orders')
+});
+
 
 module.exports = router;
